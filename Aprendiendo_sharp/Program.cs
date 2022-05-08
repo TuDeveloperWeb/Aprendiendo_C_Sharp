6d85f90636fddb2560ef38510e8374a1a2555af8
oscar <ruizoscar130495@gmail.com>
2022-05-08 00:30:37 -0500

    Condicionales

---
+++ Aprendiendo_sharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aprendiendo_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales
+
+            int x,y;
+            Console.WriteLine("Ingrese el primer numero");
+            x= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("El numero " + x + " es mayor que " + y);
+            }
+            else if(x == y)
+            {
+                Console.WriteLine("El numero "+x+" es igual que "+y);
+            }
+            else
+            {
+                Console.WriteLine("El numero " + x + " es menor que " + y);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
